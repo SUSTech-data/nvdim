c1e3bff35cbe72628eab4abd087f7bf4f18bf2c4
fecet <xiezej@gmail.com>
2024-05-05 15:07:07 +0800

    preset K

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,6 +5,12 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.keymap.set(
+    { "n", "v", "o" },
+    "K",
+    "<C-u>",
+    { desc = "Join line with smart whitespace removal" }
+)
 
 require("lazy").setup({
     pin = true,
