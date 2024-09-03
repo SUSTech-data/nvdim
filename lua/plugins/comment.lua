ee7c5c1a6c57124c058bbe42f35bd2dbbde6b922
fecet <xiezej@gmail.com>
2024-09-03 10:52:17 +0800

    use coment.nvim instead of builtin comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,20 +1,37 @@
 return {
-    "numToStr/Comment.nvim",
-    -- event = { "CursorHold", "CursorHoldI" },
-    opts = {
-        -- ignore = function()
-        --     -- Only ignore empty lines for lua files
-        --     if vim.bo.filetype == "python" then return "^(%s*)# %%" end
-        -- end,
-        -- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    {
+        "numToStr/Comment.nvim",
+        -- dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+        event = { "User IceLoad" },
+        -- keys = {
+        --     {
+        --         "gcc",
+        --         "gc",
+        --         "gb",
+        --         "gbc",
+        --         "gc0",
+        --         "gcA",
+        --     },
+        -- },
+        opts = {
+            -- ignore = function()
+            --     -- Only ignore empty lines for lua files
+            --     if vim.bo.filetype == "python" then return "^(%s*)# %%" end
+            -- end,
+            -- pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        },
+        config = function(_, opts)
+            require("Comment").setup(opts)
+            local ft = require("Comment.ft")
+            ft.javascript = { "//%s", "/*%s*/" }
+            ft.yaml = "#%s"
+            ft({ "go", "rust", "proto" }, ft.get("c"))
+            ft({ "toml", "graphql" }, "#%s")
+            ft({ "rmd", "lean3", "quarto", "qmd" }, "-- %s")
+        end,
     },
-    config = function(_, opts)
-        require("Comment").setup(opts)
-        local ft = require("Comment.ft")
-        ft.javascript = { "//%s", "/*%s*/" }
-        ft.yaml = "#%s"
-        ft({ "go", "rust" }, ft.get("c"))
-        ft({ "toml", "graphql" }, "#%s")
-        ft({ "rmd", "lean3", "quarto", "qmd" }, "-- %s")
-    end,
+    {
+        "folke/ts-comments.nvim",
+        enabled = false,
+    },
 }
