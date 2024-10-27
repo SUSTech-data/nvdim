308df296bade45863759e9fda9713f52c923c18b
fecet <xiezej@gmail.com>
2024-10-27 15:56:30 +0800

    fix: type hint in pylance

--- lua/plugins/lsp/servers/pylance.lua
+++ lua/plugins/lsp/servers/pylance.lua
@@ -16,15 +16,10 @@ return {
                 autoImportCompletions = true,
                 autoFormatStrings = false,
                 indexing = true,
-                packageIndexDepths = {
-                    { name = "vectorbtpro", depth = 10 },
-                    { name = "torch", depth = 10 },
-                    { name = "pytorch", depth = 10 },
-                },
                 inlayHints = {
                     variableTypes = true,
                     functionReturnTypes = true,
-                    callArgumentNames = true,
+                    callArgumentNames = "partial",
                     pytestParameters = true,
                 },
             },
