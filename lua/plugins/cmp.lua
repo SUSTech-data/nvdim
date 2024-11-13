4568198ea4611d752825c7ed398cbb390b806fbd
fecet <xiezej@gmail.com>
2024-11-13 14:58:15 +0800

    chore!: update to lazyvim 13 and nvim 11

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,16 +9,12 @@ return {
             "jalvesaq/cmp-zotcite",
             "saadparwaiz1/cmp_luasnip",
         },
-        keys = {
-            { "<Tab>", false, mode = { "i", "s" } },
-        },
         opts = function(_, opts)
+            table.insert(
+                opts.snippet,
+                { expand = function(args) require("luasnip").lsp_expand(args.body) end }
+            )
             local cmp = require("cmp")
-            local t = function(str) return vim.api.nvim_replace_termcodes(str, true, true, true) end
-
-            opts.snippet = {
-                expand = function(args) require("luasnip").lsp_expand(args.body) end,
-            }
 
             opts.mapping = vim.tbl_extend("force", opts.mapping, {
                 ["<C-e>"] = cmp.mapping(function(fallback)
@@ -30,25 +26,30 @@ return {
                 end),
                 ["<C-k>"] = cmp.mapping.select_prev_item(),
                 ["<C-j>"] = cmp.mapping.select_next_item(),
-                ["<CR>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        vim.api.nvim_feedkeys(t("<C-g>u"), "n", true)
-                        cmp.confirm({ select = true })
-                    else
-                        fallback()
-                    end
-                end),
+                -- ["<CR>"] = cmp.mapping(function(fallback)
+                --     if cmp.visible() then
+                --         vim.api.nvim_feedkeys(t("<C-g>u"), "n", true)
+                --         cmp.confirm({ select = true })
+                --     else
+                --         fallback()
+                --     end
+                -- end),
 
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    local luasnip = require("luasnip")
-                    if require("luasnip").expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif require("copilot.suggestion").is_visible() then
-                        require("copilot.suggestion").accept()
-                    else
-                        fallback()
+                ["<tab>"] = cmp.mapping(
+                    function(fallback)
+                        return LazyVim.cmp.map({ "snippet_forward", "ai_accept" }, fallback)()
                     end
-                end, { "i", "s" }),
+                ),
+                -- ["<Tab>"] = cmp.mapping(function(fallback)
+                --     local luasnip = require("luasnip")
+                --     if require("luasnip").expand_or_jumpable() then
+                --         luasnip.expand_or_jump()
+                --     elseif require("copilot.suggestion").is_visible() then
+                --         require("copilot.suggestion").accept()
+                --     else
+                --         fallback()
+                --     end
+                -- end, { "i", "s" }),
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     local luasnip = require("luasnip")
                     if luasnip.jumpable(-1) then
@@ -60,24 +61,23 @@ return {
             })
 
             local compare = require("cmp.config.compare")
-            local comparators = {
-                -- require("copilot_cmp.comparators").prioritize,
-                -- require("copilot_cmp.comparators").score,
-                -- require("cmp_tabnine.compare"),
-                compare.offset, -- items closer to cursor will have lower priority
-                compare.exact,
-                compare.scopes,
-                compare.sort_text,
-                compare.score,
-                compare.recently_used,
-                -- compare.locality, -- items closer to cursor will have higher priority, conflicts with `offset`
-                compare.kind,
-                compare.length,
-                compare.order,
-            }
             opts.sorting = {
                 priority_weight = 2,
-                comparators = comparators,
+                comparators = {
+                    -- require("copilot_cmp.comparators").prioritize,
+                    -- require("copilot_cmp.comparators").score,
+                    -- require("cmp_tabnine.compare"),
+                    compare.offset, -- items closer to cursor will have lower priority
+                    compare.exact,
+                    compare.scopes,
+                    compare.sort_text,
+                    compare.score,
+                    compare.recently_used,
+                    -- compare.locality, -- items closer to cursor will have higher priority, conflicts with `offset`
+                    compare.kind,
+                    compare.length,
+                    compare.order,
+                },
             }
             opts.formatting = {
                 fields = { "abbr", "kind", "menu" },
@@ -119,7 +119,7 @@ return {
                     return vim_item
                 end,
             }
-            opts.sources = cmp.config.sources({
+            vim.tbl_extend("force", opts.sources, {
                 {
                     name = "luasnip",
                     group_index = 1,
@@ -134,18 +134,12 @@ return {
                         return not string_ctx and not comment_ctx
                     end,
                 },
-                -- { name = "copilot" },
-                { name = "nvim_lsp" },
+                { name = "neopyter", option = { completers = { "CompletionProvider:kernel" } } },
+                { name = "buffer" },
                 { name = "cmp_zotcite" },
                 { name = "jupynium" },
-                -- { name = "neopyter" },
-                { name = "neopyter", option = { completers = { "CompletionProvider:kernel" } } },
-                { name = "codeium" },
-                { name = "nvim_lua" },
                 { name = "path" },
-                { name = "treesitter" },
                 { name = "spell" },
-                { name = "buffer" },
             })
         end,
 
@@ -168,6 +162,54 @@ return {
                     { name = "cmdline_history" },
                 }),
             })
+        end,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        event = "BufRead",
+        dependencies = {
+            { "fecet/vim-snippets" },
+            {
+                -- "fecet/luasnips-mathtex-snippets",
+                "fecet/luasnip-latex-snippets.nvim",
+                dependencies = {
+                    "preservim/vim-markdown",
+                    -- "lervag/vimtex",
+                },
+                ft = { "markdown", "rmd", "quarto", "norg" },
+                config = function()
+                    -- vim.g.tex_conceal = "abdmgs"
+                    -- vim.g.tex_flavor = "latex"
+                    -- vim.g.vim_markdown_math = 1
+                    -- vim.g.vim_markdown_folding_disabled = 1
+
+                    require("luasnip-latex-snippets").setup({
+                        use_treesitter = true,
+                        allow_on_markdown = false,
+                    })
+                    require("luasnip-latex-snippets").setup_markdown()
+                end,
+            },
+        },
+        config = function()
+            local luasnip = require("luasnip")
+            local markdown_family = { "markdown", "rmd", "quarto", "qmd", "norg" }
+
+            require("luasnip.loaders.from_lua").lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip.loaders.from_snipmate").lazy_load()
+            for i = 2, #markdown_family do
+                luasnip.filetype_extend(markdown_family[i], { "markdown" })
+            end
+            luasnip.filetype_extend("quarto", { "qmd" })
+            local opts = {}
+            luasnip.config.setup({
+                update_events = "TextChanged,TextChangedI",
+                delete_check_events = "TextChanged,InsertLeave",
+                enable_autosnippets = true,
+                store_selection_keys = "<tab>",
+            })
+            luasnip.config.setup(opts)
         end,
     },
 }
