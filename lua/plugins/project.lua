ceabc7bf0ee6121257c75c29a69fea99387a3638
fecet <xiezej@gmail.com>
2024-12-05 16:21:16 +0800

    feat: nav

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -27,7 +27,6 @@ return {
     },
     {
         "olimorris/persisted.nvim",
-
         cmd = {
             "SessionToggle",
             "SessionStart",
@@ -60,7 +59,32 @@ return {
     {
         "folke/persistence.nvim",
         enabled = false,
-        event = "BufReadPre",
-        opts = {},
+    }, -- Lazy.nvim
+    {
+        "you-n-g/navigate-note.nvim",
+        event = "BufRead",
+        opts = {
+            filename = "nav.md", -- The filename of the markdown.
+            width = 0.6, -- The width of the popup window when jumping in the file with <tab>.
+            keymaps = {
+                nav_mode = {
+                    -- Navigation & Jumping
+                    next = "<tab>",
+                    prev = "<s-tab>",
+                    open = "<cr>",
+                    switch_back = "<leader>nb", -- Switch back to the previous file from `nav.md`.
+                    -- Editing
+                    append_link = "<leader>np", -- (P)aste will more align with the meaning.
+                    -- Mode switching
+                    -- jump_mode = "<m-l>", -- When we jump to a file, jump to the file only or jump to the exact file:line.
+                },
+                add = "<leader>na",
+                open_nav = "<leader>nn", -- Switch to `nav.md`.
+            },
+            context_line_count = { -- It would be a total of `2 * context_line_count - 1` lines.
+                tab = 4,
+                vline = 5,
+            },
+        },
     },
 }
