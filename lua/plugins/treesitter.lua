97ca2547ca9093e8766f6f5438e8b63936e0b8d5
fecet <xiezej@gmail.com>
2024-12-05 16:17:48 +0800

    style: move comment

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -98,6 +98,17 @@ return {
                     },
                 },
             },
+            {
+                "folke/ts-comments.nvim",
+                opts = {
+                    just = "# %s",
+                    rmd = "-- %s",
+                    lean3 = "-- %s",
+                    quarto = "-- %s",
+                    toml = "# %s",
+                    graphql = "# %s",
+                },
+            },
         },
     },
 }
