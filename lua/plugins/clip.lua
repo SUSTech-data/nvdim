f89c5a7e3d0be7a286b5e7a505806328eccb8b10
fecet <xiezej@gmail.com>
2024-12-08 14:14:41 +0800

    feat: lazycp

---
+++ lua/plugins/clip.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "atiladefreitas/lazyclip",
+        config = function() require("lazyclip").setup() end,
+        keys = {
+            {
+                "<leader>cp",
+                ":lua require('lazyclip').show_clipboard()<CR>",
+                desc = "Open Clipboard Manager",
+            },
+        },
+    },
+}
+
