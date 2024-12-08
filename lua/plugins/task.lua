f58b90c65cdd6acbcb618b793dfd8915742dcd2a
fecet <xiezej@gmail.com>
2024-12-08 14:08:10 +0800

    feat: just fmt

--- lua/plugins/task.lua
+++ lua/plugins/task.lua
@@ -55,4 +55,22 @@ return {
             },
         },
     },
+    {
+        "stevearc/conform.nvim",
+        keys = {
+            { "<leader>cF", false },
+            {
+                "<leader>fm",
+                -- function() require("conform").format({ formatters = { "injected" } }) end,
+                "<cmd>LazyFormat<CR>",
+                mode = { "n", "v" },
+                desc = "Format Injected Langs",
+            },
+        },
+        opts = {
+            formatters_by_ft = {
+                just = { "just" },
+            },
+        },
+    },
 }
