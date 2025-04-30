0f47e366fbe1f8b99ecd3a59550f53e772b08dea
fecet <xiezej@gmail.com>
2025-04-30 13:48:16 +0800

    refactor: remove ai and jupyter in favor of vicode

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -3,34 +3,6 @@ if vim.g.vscode then return {} end
 
 -- if vim.g.swenv == nil then vim.g.swenv = "dl" end
 local file_pattern = { "*.ju.*", "*_jnb.*" }
-local insert_cell = function(above)
-    local cells = require("jupynium.cells")
-    local current_separator_row = cells.current_cell_separator()
-    if current_separator_row == nil then return end
-    local start_row = current_separator_row
-    local next_row = cells.next_cell_separator()
-    local end_row = next_row and next_row or vim.api.nvim_buf_line_count(0)
-    -- print(start_row, end_row)
-    local row = above and start_row or end_row
-    vim.api.nvim_buf_set_lines(0, row - 1, row - 1, false, { "# %%", "" })
-    vim.api.nvim_win_set_cursor(0, { row + 1, 0 })
-end
-
--- vim.api.nvim_create_autocmd("FileType", {
---     pattern = { "python" },
---     callback = function()
---         if vim.g.vscode then return end
---
---         if vim.env.CONDA_PREFIX == nil then
---             if vim.g.VirtualEnvironment then
---                 require("swenv.api").set_venv(vim.g.VirtualEnvironment)
---             else
---                 require("swenv.api").set_venv("base")
---                 -- require("swenv.api").auto_venv()
---             end
---         end
---     end,
--- })
 
 -- local tool = "jupynium"
 local tool = "neopyter"
@@ -80,8 +52,6 @@ local jupyter_callback = function(buf_id)
                 "<cmd>lua require'jupynium.textobj'.goto_next_cell_separator()<cr>",
                 { desc = "next" },
             },
-            { "o", function() insert_cell(false) end, { exit = true } },
-            { "O", function() insert_cell(true) end, { exit = true } },
             {
                 "v",
                 "<cmd>lua require'jupynium.textobj'.select_cell(false, false)<cr>",
@@ -203,46 +173,10 @@ return {
     },
     {
         "sustech-data/neopyter",
+        enabled = false,
         cond = tool == "neopyter",
         dependencies = {
             "AbaoFromCUG/websocket.nvim",
-            {
-                "kiyoon/jupynium.nvim",
-                opts = {
-                    jupynium_file_pattern = file_pattern,
-                    python_host = (vim.env.MICROMAMBA_ROOT_PREFI or "~/.conda") .. "/bin/python",
-                    default_notebook_URL = "localhost:8888",
-                    firefox_profiles_ini_path = "~/.mozilla/firefox/profiles.ini",
-                    firefox_profile_name = nil,
-                    auto_start_server = {
-                        enable = false,
-                        file_pattern = file_pattern,
-                    },
-                    auto_attach_to_server = {
-                        enable = false,
-                        file_pattern = file_pattern,
-                    },
-                    auto_start_sync = {
-                        enable = false,
-                        file_pattern = file_pattern,
-                    },
-                    auto_download_ipynb = false,
-                    autoscroll = {
-                        enable = false,
-                        mode = "always", -- "always" or "invisible"
-                        cell = {
-                            top_margin_percent = 20,
-                        },
-                    },
-
-                    use_default_keybindings = false,
-                    textobjects = {
-                        use_default_keybindings = false,
-                    },
-                    shortsighted = false,
-                    auto_close_tab = false,
-                },
-            },
         },
         opts = {
             filename_mapper = function(ju_path)
@@ -276,5 +210,54 @@ return {
             vim.g.molten_image_provider = "image.nvim"
             vim.g.molten_output_win_max_height = 20
         end,
+    },
+    {
+        "kiyoon/jupynium.nvim",
+        keys = {
+            {
+                "<C-f>",
+                function() require("jupynium.textobj").goto_next_cell_separator() end,
+                mode = { "n", "x", "o" },
+            },
+            {
+                "<C-b>",
+                function() require("jupynium.textobj").goto_previous_cell_separator() end,
+                mode = { "n", "x", "o" },
+            },
+        },
+        opts = {
+            jupynium_file_pattern = file_pattern,
+            python_host = (vim.env.MICROMAMBA_ROOT_PREFI or "~/.conda") .. "/bin/python",
+            default_notebook_URL = "localhost:8888",
+            firefox_profiles_ini_path = "~/.mozilla/firefox/profiles.ini",
+            firefox_profile_name = nil,
+            auto_start_server = {
+                enable = false,
+                file_pattern = file_pattern,
+            },
+            auto_attach_to_server = {
+                enable = false,
+                file_pattern = file_pattern,
+            },
+            auto_start_sync = {
+                enable = false,
+                file_pattern = file_pattern,
+            },
+            auto_download_ipynb = false,
+            autoscroll = {
+                enable = false,
+                mode = "always", -- "always" or "invisible"
+                cell = {
+                    top_margin_percent = 20,
+                },
+            },
+
+            use_default_keybindings = false,
+            textobjects = {
+                use_default_keybindings = false,
+            },
+            shortsighted = false,
+            auto_close_tab = false,
+        },
     },
 }
